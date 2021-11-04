b78fdd59c430866d7ad33d046415e8061f291b4b
Richard Navarro <richardnavarro559@gmail.com>
2021-11-03 17:33:00 -0700

    Homework

--- Day 1/js/test.js
+++ Day 1/js/test.js
@@ -1,2 +1,4 @@
-console.log('Richard Navarro')
-console.log('Asian Fusion spicy');
+const firstName ='Richard';
+const food = 'Spicy Asian Fusion';
+console.log("First Name", firstName)
+console.log("Favorite Food", food)
